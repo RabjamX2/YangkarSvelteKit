e1dc03accb8bd8d42f98b5f13ca0780267581134
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2025-09-26 06:10:02 -0400

    Update deleteAll.cjs

--- apps/backend/scripts/deleteAll.cjs
+++ apps/backend/scripts/deleteAll.cjs
@@ -6,30 +6,23 @@ const tableNameList2 = [
     "StockChange",
     "PurchaseOrderItem",
     "PurchaseOrder",
-    "ProductVariant",
-    "Product",
     "CustomerOrderItem",
     "CustomerOrder",
     "Customer",
+    "ProductVariant",
+    "Product",
 ];
 const tableNameList = ["CustomerOrderItem", "CustomerOrder", "Customer"];
-// const tableNameList3 = [
-//     "InventoryBatch",
-//     "StockChange",
-//     "PurchaseOrderItem",
-//     "PurchaseOrder",
-//     "ProductVariant",
-//     "Product",
-//     "CustomerOrderItem",
-//     "CustomerOrder",
-//     "Customer",
-// ];
+const tableNameList3 = ["InventoryBatch", "StockChange", "CustomerOrderItem", "CustomerOrder", "Customer"];
 
 async function main() {
     for (const tableName of tableNameList2) {
-        // await prisma.$executeRawUnsafe(`TRUNCATE TABLE "${tableName}" RESTART IDENTITY;`);
         await prisma.$executeRawUnsafe(`DELETE FROM "${tableName}" CASCADE`);
         await prisma.$executeRawUnsafe(`ALTER SEQUENCE "${tableName}_id_seq" RESTART WITH 1`);
+        // Set all entries in a table to null for a specific column
+        if (tableName === "InventoryBatch") {
+            await prisma.$executeRawUnsafe(`UPDATE "PurchaseOrder" SET "hasArrived" = false`);
+        }
         console.log("All entries deleted and ID sequence reset for table:", tableName);
     }
 }
